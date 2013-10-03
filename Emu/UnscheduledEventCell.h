60c2867b388ca8a61262c1f24d1ef651b127e313
Christopher Constable <chris.m.constable@gmail.com>
2013-10-03 15:04:37 -0400

    Added unscheduled event cell.

---
+++ Emu/UnscheduledEventCell.h
@@ -0,0 +1,13 @@
+//
+//  UnscheduledEventCell.h
+//  Emu
+//
+//  Created by Christopher Constable on 10/3/13.
+//  Copyright (c) 2013 Mobiquity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UnscheduledEventCell : UITableViewCell
+
+@end
---
+++ Emu/UnscheduledEventCell.m
@@ -0,0 +1,29 @@
+//
+//  UnscheduledEventCell.m
+//  Emu
+//
+//  Created by Christopher Constable on 10/3/13.
+//  Copyright (c) 2013 Mobiquity. All rights reserved.
+//
+
+#import "UnscheduledEventCell.h"
+
+@implementation UnscheduledEventCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
